817c1587828132e9fac4b748f0b233864b53de3a
tmt-films <amxrrical@gmail.com>
2025-06-08 18:37:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 MONGO_URL = os.getenv("MONGO_URL")
 
-ADMIN_USER_IDS = [123456789] # TODO: Replace with actual admin user ID(s)
+ADMIN_USER_IDS = [7183781201] # TODO: Replace with actual admin user ID(s)
